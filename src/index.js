e8370e085f517e4cbaa5f7f3c9eb26fc0f493b07
cfiander <cfiander90@gmail.com>
2019-08-13 10:15:26 -0700

    context written

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { JobReelProvider } from './context/JobReelContext'
 import './index.css';
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <JobReelProvider>
+            <App />
+        </JobReelProvider>
     </BrowserRouter>,
     document.getElementById('root'),
-  )
-  
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
