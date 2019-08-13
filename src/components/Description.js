96d09ce714c4aa71a66e159100a7e348d9efb419
Erin Dustin <erincdustin@gmail.com>
2019-08-13 09:05:59 -0400

    react router set up with description component in landing route

---
+++ src/components/Description.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Description.css'
+
+export default function Description(props){
+  return(
+    <section className="app-description">
+      <h1>JobReel</h1>
+      <h2>description text</h2>
+    </section>
+  )
+}
---
+++ src/routes/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Description from '../components/Description';
+
+export default class Landing extends React.Component{
+  render(){
+    return(
+      <section className="landing">
+        <Description />
+      </section>
+    )
+  }
+}
