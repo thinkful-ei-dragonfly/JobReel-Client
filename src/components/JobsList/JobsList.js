71175780dfaebd064f127048ef4425057a2f9f36
cfiander <cfiander90@gmail.com>
2019-08-14 15:42:13 -0700

    added JobsListand Job Details, jobreel-api-service, and new context objects and methods. Temp data in place of fetch data

---
+++ src/components/JobsList/JobsList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import JobReelApiService from '../../services/jobreel-api-service'
+import JobReelContext from '../../context/JobReelContext';
+import Job from '../Job/Job'
+export default class JobsList extends Component {
+    static contextType = JobReelContext
+
+    componentDidMount() {
+
+        setTimeout(() => {
+            const search = this.context.search
+            const jobs = JobReelApiService.getJobs(search)
+            this.context.setJobData(jobs)
+            console.log(this.context)
+                // .then(this.context.setJobs)
+                // .catch(this.context.setError)
+        }, 200)
+    }
+
+    renderJobList() {
+        const {jobData = {}} = this.context
+        const {jobs = {} } = this.context
+        console.log(jobs)
+        const jobsList= jobs.map(job => {
+            return <Job job={job} />
+        })
+        return (
+            <>
+            Total Results: {jobData.totalResults}
+            <br/>
+            Page Number: {jobData.pageNumber+1}
+            <br/>
+            JobsList:
+            {jobsList}
+            </>
+        )
+    }
+    render() {
+        return (
+            <div>
+                {this.renderJobList()}
+            </div>
+        )
+    }
+}
