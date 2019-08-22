d11c5b05a6e866f6de3446ff70fb1686121b56fe
Michael Romero <michael.romero09@gmail.com>
2019-08-22 10:41:15 -0700

    Changed api_endpoint in config.js file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.API_ENDPOINT || 'https://stormy-beyond-18995.herokuapp.com/api',
+    // API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
     // 'https://stormy-beyond-18995.herokuapp.com/api',
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_TOKEN: process.env.CLIENT_TOKEN,
