69e485dbc07f5dcbf6459254823aaff64bd3f5fa
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-22 12:52:04 -0500

    Completed Job testing

---
+++ src/components/Job/Job.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Job from './Job'
+
+describe('Job smoke test', () => {
+  const props = {
+    job:{
+      company: 'Company'
+    }
+  }
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Job {...props} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
