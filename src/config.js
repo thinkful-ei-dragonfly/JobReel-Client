9446923621774b59bcd6ca38cc5c04b4e6137e3c
cfiander <cfiander90@gmail.com>
2019-08-25 17:58:13 -0700

    asdf

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || 'https://rocky-crag-74904.herokuapp.com/api',
+    API_ENDPOINT: process.env.API_ENDPOINT || 'https://warm-bastion-62347.herokuapp.com/api',
     // API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_TOKEN: process.env.CLIENT_TOKEN,
-    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'jobReel-jwt-secret',
+    REACT_APP_TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'jobReel-jwt-secret',
   }
   
