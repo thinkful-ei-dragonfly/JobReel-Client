0e8f3ac3685645267b07900f012524049f3e4607
Erin Dustin <erincdustin@gmail.com>
2019-08-27 11:26:52 -0400

    changed API endpoint back to heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-    // API_ENDPOINT: process.env.API_ENDPOINT || 'https://stormy-beyond-18995.herokuapp.com/api',
-    API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.API_ENDPOINT || 'https://stormy-beyond-18995.herokuapp.com/api',
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_TOKEN: process.env.CLIENT_TOKEN,
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'jobreel-client-auth-token',
