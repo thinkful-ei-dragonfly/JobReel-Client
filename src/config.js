6f704323acf42e873d2e556f312856dd7c234202
cfiander <cfiander90@gmail.com>
2019-08-30 10:26:32 -0700

    changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 export default {
     API_ENDPOINT: process.env.API_ENDPOINT || 'https://stormy-beyond-18995.herokuapp.com/api',
-    // API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_TOKEN: process.env.CLIENT_TOKEN,
     REACT_APP_TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'jobReel-jwt-secret',
   }
   
