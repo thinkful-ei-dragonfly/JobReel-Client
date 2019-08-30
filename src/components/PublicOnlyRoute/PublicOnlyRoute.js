b9c0b5593bfc893c74ee15f7a32130cc2c19c648
Jonathan Moore <jonathan.moore6247@gmail.com>
2019-08-30 12:15:31 -0400

    Fixed redirect

--- src/components/PublicOnlyRoute/PublicOnlyRoute.js
+++ src/components/PublicOnlyRoute/PublicOnlyRoute.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import JobReelContext from '../../context/JobReelContext'
+import JobReelContext from '../../context/JobReelContext';
 
 export default function PublicOnlyRoute({ component, ...props }) {
   const Component = component
